b5d530a668f3725378d0e641a75de7c82cf287f9
Timothy <timothy.hioe@stud.h-da.de>
2024-11-18 15:32:41 +0100

    add cube class

---
+++ src/Game/Cube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by LENOVO on 18/11/2024.
+//
+
+#include "Cube.h"
---
+++ src/Game/Cube.h
@@ -0,0 +1,14 @@
+//
+// Created by LENOVO on 18/11/2024.
+//
+
+#ifndef OPENGL_PRAKTIKUM_CUBE_H
+#define OPENGL_PRAKTIKUM_CUBE_H
+
+
+class Cube {
+
+};
+
+
+#endif //OPENGL_PRAKTIKUM_CUBE_H
